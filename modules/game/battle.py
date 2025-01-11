bc1222ce474b17da7fe836084a8b6106f04ed441
тима зеленый <timagreen2010@gmail.com>
2025-01-11 19:06:29 +0100

    renewed structure of the main script new finder

--- modules/game/battle.py
+++ modules/game/battle.py
@@ -367,8 +367,20 @@ def battle():
         if shot_type == 100:
             print("miss")  
 
-        if shot_type == 10:
-            print("gooooood")    
+        if shot_type == 21:
+            print("trio left gor")    
+
+        if shot_type == 22:
+            print("trio right gor") 
+
+        if shot_type == 23:
+            print("trio center ver") 
+
+        if shot_type == 24:
+            print("trio left ver") 
+
+        if shot_type == 25:
+            print("trio right ver") 
 
         if shot_type == 20:
             if row != 0:
@@ -490,6 +502,10 @@ def battle():
             if cell != 9 and row != 9:
                 add_miss(list, number, 11, "plus")
          
+    
+    def check_next_cell():
+        pass
+    
     def radar(list, list2, row, cell, type):
         print(list2, row, cell, type)
 
@@ -517,12 +533,20 @@ def battle():
         # 1-2 12 duo top
         # 1-3 13 duo right
         # 1-4 14 duo down
-        # 2-0 20 trio center
+        # 2-0 20 trio center gor
+        # 2-0 21 trio left gor
+        # 2-0 22 trio right gor
+        # 2-0 23 trio center ver
+        # 2-0 24 trio left ver
+        # 2-0 24 trio right ver
+
+
         
         clean = radar(list, clean_side_list, row, cell, 0)
         ship = radar(list, ship_side_list, row, cell, 1)
         dead_ship = radar(list, dead_ship_side_list, row, cell, 2)
 
+        print("CLICK")
         print(clean)
         print(ship)
         print(dead_ship)
@@ -530,64 +554,171 @@ def battle():
         if all(meaning for meaning in clean) or all(not meaning for meaning in ship) and all(not meaning for meaning in dead_ship):
             return 1
         
-        
-        number = 0
-        for dead in dead_ship:
-            print(dead)
-
-            if dead and number == 0:
-                clean = radar(list, clean_side_list, row, cell + 1, 0)
-                ship = radar(list, ship_side_list, row, cell + 1, 1)
-                dead_ship = radar(list, dead_ship_side_list, row, cell + 1, 2)
-
-                print(clean)
-                print(ship)
-                print(dead_ship)
-
-                if cell != 9 and clean[1] == 2 and clean[1] or cell == 9:
-                    return 13
+        for index, dead in enumerate(dead_ship) :
+            print(dead, index)
                 
-            if dead and number == 1:
-                clean = radar(list, clean_side_list, row, cell - 1, 0)
-                ship = radar(list, ship_side_list, row, cell - 1, 1)
-                dead_ship = radar(list, dead_ship_side_list, row, cell - 1, 2)
+            if cell != 0 and dead and index == 0 and clean[1]:
+                new_cell = cell + 1
+                clean = radar(list, clean_side_list, row, new_cell, 0)
+                ship = radar(list, ship_side_list, row, new_cell, 1)
+                dead_ship = radar(list, dead_ship_side_list, row, new_cell, 2)
 
                 print(clean)
                 print(ship)
                 print(dead_ship)
 
-                if cell != 9 and clean[1] == 2 and clean[1] or cell == 9:
+                if new_cell == 9:
+                    return 11
+
+                if new_cell != 9 and clean[0]:
                     return 11
                 
-            if dead and number == 2:
-                clean = radar(list, clean_side_list, row+ 1, cell, 0)
-                ship = radar(list, ship_side_list, row+ 1, cell, 1)
-                dead_ship = radar(list, dead_ship_side_list+ 1, row, cell, 2)
+                if new_cell !=9 and dead_ship[0]:
+                    new_cell = cell + 2
+                    clean = radar(list, clean_side_list, row, new_cell, 0)
+                    ship = radar(list, ship_side_list, row, new_cell, 1)
+                    dead_ship = radar(list, dead_ship_side_list, row, new_cell, 2)
+
+                    print(clean)
+                    print(ship)
+                    print(dead_ship)
+
+                    if new_cell == 9:
+                        print("aso;fghso;\npapadpapaa\n spsfipspgspjgspj")
+                        return 21
+
+                    if new_cell != 9 and clean[0]:
+                        print("aso;fghso;\npapadpapaa\n spsfipspgspjgspj")
+                        return 21
+                    
+                    if new_cell !=9 and dead_ship[0]:
+                        new_cell = cell + 3
+                        clean = radar(list, clean_side_list, row, new_cell, 0)
+                        ship = radar(list, ship_side_list, row, new_cell, 1)
+                        dead_ship = radar(list, dead_ship_side_list, row, new_cell, 2)
+
+                        print(clean)
+                        print(ship)
+                        print(dead_ship)
+
+                        if new_cell == 9:
+                            print("44444444444")
+                            # return 21
+
+                        if new_cell != 9 and clean[0]:
+                            print("555555555555555555555555555")
+                            # return 21
+            #########################################################
+            if cell == 0 and dead and index == 0:
+                new_cell = cell + 1
+                clean = radar(list, clean_side_list, row, new_cell, 0)
+                ship = radar(list, ship_side_list, row, new_cell, 1)
+                dead_ship = radar(list, dead_ship_side_list, row, new_cell, 2)
 
                 print(clean)
                 print(ship)
                 print(dead_ship)
 
-                if row != 9 and clean[1] == 2 and clean[1] or row == 9:
+                if new_cell == 9:
+                    return 11
+
+                if new_cell != 9 and clean[0]:
+                    return 11
+                
+                if new_cell !=9 and dead_ship[0]:
+                    new_cell = cell + 2
+                    clean = radar(list, clean_side_list, row, new_cell, 0)
+                    ship = radar(list, ship_side_list, row, new_cell, 1)
+                    dead_ship = radar(list, dead_ship_side_list, row, new_cell, 2)
+
+                    print(clean)
+                    print(ship)
+                    print(dead_ship)
+
+                    if new_cell == 9:
+                        print("aso;fghso;\npapadpapaa\n spsfipspgspjgspj")
+                        return 21
+
+                    if new_cell != 9 and clean[0]:
+                        print("aso;fghso;\npapadpapaa\n spsfipspgspjgspj")
+                        return 21
+                    
+                    if new_cell !=9 and dead_ship[0]:
+                        new_cell = cell + 3
+                        clean = radar(list, clean_side_list, row, new_cell, 0)
+                        ship = radar(list, ship_side_list, row, new_cell, 1)
+                        dead_ship = radar(list, dead_ship_side_list, row, new_cell, 2)
+
+                        print(clean)
+                        print(ship)
+                        print(dead_ship)
+
+                        if new_cell == 9:
+                            print("44444444444")
+                            # return 21
+
+                        if new_cell != 9 and clean[0]:
+                            print("4444444444444444444444444444444444444")
+                            # return 21
+
+            
+            
+            
+            
+            
+            if cell != 9 and dead and index == 1 and clean[0]:
+                new_cell = cell - 1
+                print(row, cell, new_cell)
+                clean = radar(list, clean_side_list, row, new_cell, 0)
+                ship = radar(list, ship_side_list, row, new_cell, 1)
+                dead_ship = radar(list, dead_ship_side_list, row, new_cell, 2)
+
+                
+                print("NEW")
+                print(clean)
+                print(ship)
+                print(dead_ship)
+
+                if new_cell == 0:
+                    print("cell == 9")
+                    return 13
+
+                elif new_cell != 0 and clean[1]:
+                    print("cell != 9 and ...")
+                    return 13
+                
+            if dead and index == 2:
+                new_row = row + 1
+                clean = radar(list, clean_side_list, new_row, cell, 0)
+                ship = radar(list, ship_side_list, new_row, cell, 1)
+                dead_ship = radar(list, dead_ship_side_list, new_row, cell, 2)
+
+                print(clean)
+                print(ship)
+                print(dead_ship)
+
+                if new_row != 9:
+                    return 12
+
+                if row != 9 and clean[2]:
                     return 12
                 
-            if dead and number == 3:
-                clean = radar(list, clean_side_list, row - 1, cell, 0)
-                ship = radar(list, ship_side_list, row - 1, cell, 1)
-                dead_ship = radar(list, dead_ship_side_list, row - 1, cell, 2)
+            if dead and index == 3:
+                new_row = row - 1
+                clean = radar(list, clean_side_list, new_row, cell, 0)
+                ship = radar(list, ship_side_list, new_row, cell, 1)
+                dead_ship = radar(list, dead_ship_side_list, new_row, cell, 2)
 
                 print(clean)
                 print(ship)
                 print(dead_ship)
 
-                if row != 9 and clean[1] == 2 and clean[1] or row == 9:
+                if new_row != 0:
                     return 14
-                
-                
-                
+
+                if new_row != 0 and clean[3]:
+                    return 14
             
-            number += 1
-        
         else:
             return 100
    
